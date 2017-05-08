719d66f76b5f05191ff2f6ab527a568c0ccaa191
feliciar <feliciar@kth.se>
2017-05-08 13:54:35 +0200

    Added median price, and refractored some

--- js/display_data.js
+++ js/display_data.js
@@ -1,24 +1,42 @@
 //search('Ford');
 
+function sortNumber(a,b) {
+    return a - b;
+}
+
 function displayData(data){
 	var count = data.length < 20 ? data.length : 20;
 	var average_price = 0;
+	var mean_price = 0;
 	var min_price = 0;
 	var max_price = 0;
 	if(count > 0){
+		
 		var i = 0;
 		min_price = 99999999;
 		max_price = -9999999;
+		var prices = [];
 		for(i = 0; i < count; i++){
+			prices[i] = parseInt(data[i]._source.price);
 			average_price += data[i]._source.price;
-			if(min_price > data[i]._source.price){
-				min_price = data[i]._source.price;
-			}
-			if(max_price < data[i]._source.price){
-				max_price = data[i]._source.price;
-			}
 		}
+
+		prices.sort(sortNumber);
+
+		min_price = prices[0];
+		max_price = prices[count-1];
+		if(count % 2 == 0) {
+			mean_price = prices[count/2-1];	
+			mean_price += prices[count/2];	
+			mean_price /= 2;
+		} else {
+			mean_price = prices[Math.round(count/2)-1];
+		}
+		
 		average_price /= count;
+
+		console.log(prices);
+
 	}
 	console.log(min_price);
 	console.log(max_price);
@@ -31,6 +49,11 @@ function displayData(data){
 	var text_0 = document.createTextNode("Average price: " + average_price + " kr.");
 	para_0.appendChild(text_0);
 
+	var para = document.createElement("p");
+	var text = document.createTextNode("Mean price: " + mean_price + " kr.");
+	para.appendChild(text);
+
+
 	var para_1 = document.createElement("p");
 	var text_1 = document.createTextNode("Min price: " + min_price + " kr.");
 	para_1.appendChild(text_1);
@@ -42,7 +65,9 @@ function displayData(data){
 	var div = document.getElementById("priceStatsContent");
 	while(div.firstChild)
 		div.removeChild(div.firstChild);
+
 	div.appendChild(para_0);
+	div.appendChild(para);
 	div.appendChild(para_1);
 	div.appendChild(para_2);
 }
